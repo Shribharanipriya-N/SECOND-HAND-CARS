97e643d5bb491889dcc03c3d7f14a62b10252e1d
Shribharanipriya-N <shribharanipriya2004@gmail.com>
2025-02-20 11:48:25 +0530

    project struture setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-blue-700  text-center">
+        Vite + React
+      </h1>
+   
+    </>
+  )
+}
+
+export default App
